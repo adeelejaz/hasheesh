a91c39a2582c90d291abc61c25a2536076919f35
Adeel Ejaz <adeel.e@gmail.com>
2012-05-14 18:20:13 +0100

    Lots of improvements.. still a few things to left

--- hasheesh.js
+++ hasheesh.js
@@ -1,4 +1,4 @@
-(function($, window, undefined){
+(function(doc, window, $, undefined){
   window.hasheesh = {
     
     // Namespace to look at
@@ -16,31 +16,62 @@
       if ( typeof hasheesh.namespace[hashObj] === "object" && typeof hasheesh.namespace[hashObj].init === "function" ) {
         hasheesh.namespace[hashObj].init();
       } else {
-        $.ajax({
-          url: "js/app/" + hashObj + "/init.js",
-          dataType: "script",
-          success: function() {
-            hasheesh.namespace[hashObj].init();
-          }
-        });
+        var el = doc.createElement('script'),
+          script = doc.getElementsByTagName('script')[0];
+
+        el.src = "js/app/" + hashObj + "/init.js";
+        script.parentNode.insertBefore(el, script);
+
+        if ( !hasheesh.namespace[hashObj] ) {
+          hasheesh.waitingFor = hashObj;
+          hasheesh.timeout = setTimeout("hasheesh.loadModule()", 500);
+        }
       }
 
     },
 
-    // Helper function to load templates
-    loadTemplate: function(file, context, callback) {
+    loadModule: function() {
 
-      if ( !context ) {
-        context = "#container";
+      if ( !hasheesh.waitingFor ) {
+        if ( hasheesh.timeout ) {
+          clearTimeout( hasheesh.timeout );
+        }
+        return;
       }
 
+      if ( hasheesh.namespace[hasheesh.waitingFor] ) {
+
+        var hash = hasheesh.waitingFor;
+        
+        // Object found, clear first
+        hasheesh.waitingFor = null;
+        clearTimeout( hasheesh.timeout );
+
+        // Now call init()
+        hasheesh.namespace[hash].init();
+        return;
+      }
+
+      hasheesh.timeout = setTimeout("hasheesh.loadModule()", 500);
+    },
+
+    // Helper function to load templates
+    loadTemplate: function(opts) {
+
+      var defaults = {
+            file: null,
+            context: "#container",
+            callback: null
+          },
+          options = $.extend(defaults, opts || {});
+
       $.ajax({
-        url: "js/app/" + file + ".html",
+        url: "js/app/" + options.file + ".html",
         dataType: "html", 
         success: function(template) {
-          $( context ).html( template );
-          if ( $.isFunction(callback) ) {
-            callback();
+          $( options.context ).html( template );
+          if ( $.isFunction( options.callback) ) {
+            options.callback();
           }
         }
       });
@@ -50,4 +81,4 @@
 
   // Get the app hooked to hasheesh
   window.onhashchange = hasheesh.load;
-})(jQuery, window);
+})(document, window, jQuery);
